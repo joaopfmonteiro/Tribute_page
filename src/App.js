cbeb8d5cec9b903cf16acd8d13d51a0d0b615349
Joao Pereira Ferreira Monteiro <96503806+joaopfmonteiro@users.noreply.github.com>
2022-02-10 19:06:32 +0000

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+  <header>
+    <div id="title">
+      <h1>Aristides de Sousa Mendes</h1>
+      <h2><em>The hero without cape</em></h2>
     </div>
+  </header>
+  <content>
+    <div>
+      <div id="img-div">
+        <img id="image" src="https://www.centerofportugal.com/transform/containers/images/aristides-principal.jpg/05800f5881bdd8b11dfcc163eb50b9b9.jpg" alt="aristides img"/>
+        <div id="img-caption"><em>Aristides de Sousa Mendes in front of one of thousands of visas issued.</em></div>
+      </div>
+      <body>
+        <h1 class="name">
+          Aristides de Sousa Mendes
+        </h1>
+        <div id="tribute-info">
+          <p class="paragraph">This page pays a tribute to 
+one of the greatest portuguese ever who made history by saving 30.000 lives from horror of the Holocaust: Aristides de Sousa Mendes (1885-1954). “The one who saves a life is saving the entire mankind”.
+Aristides de Sousa Mendes (July 18,1885 – April 3,1954) was a prestigious Portuguese diplomat who has saved 30.000 lives from the Holocaust, frantically issuing them visas either individual or for families. From June 16th to 23rd 1940, Aristides de Sousa Mendes disobeyed the rules imposed by dictator Salazar and followed his own inner moral conscience.</p>
+          <div class="title">How This History Begins </div>
+          <p class="paragraph">Aristides de Sousa Mendes do Amaral e Abranches was born on July 19th 1885 in Cabanas de Viriato near Viseu. Son of  Maria Angelina Ribeiro de Abranches and the judge José de Sousa Mendes, he graduated in Law along with his twin brother César in the University of Coimbra, by the time he was 22 years-old. In 1908 he marries his cousin Angelina, who would be the mother of his 14 children. He begins his diplomatic career quite young and in 1910 he becomes consul of Demerara in the British Guiana. He worked as a consul in the British Guiana, in Zanzibar, in Brazil (Curitiba and Porto Alegre), in the United States (San Francisco and Boston), in Spain (Vigo), in Luxemburg, in Belgium and finally in France (Bordeaux). He was a family man and a patriarch who has always had his wife and children nearby, providing them university education along with painting, drawing and music classes. One of his sons once said: ”We had a true chamber orchestra in our home and we regularly invited persons to watch our concerts. We played Chopin, Mozart, Bach, Beethoven, and so on.”
+           </p>
+          <div class="title">World War II</div>
+          <p class="paragraph">During World War II under the dictatorship of Salazar, Portugal was allegedly a so-called “neutral” nation, however it was clearly and unofficially pro-Hitler. The Portuguese government issued the wicked “Circular 14” to all its diplomats to deny safe haven to refugees, including explicitly Jews, Russians and stateless persons. But a man defied these dreadful orders and raised the voice of his consciousness, saving 30.000 persons from a certain death. Aristides de Sousa Mendes was severely punished by Salazar, who deprived him from his job and from any form of earning a living, which proved to be tragic, since Sousa Mendes had 15 children, who were blacklisted and prevented to attend university. On June 16th 1940 the Portuguese consul in Bordeaux, Aristides de Sousa Mendes, meets the Polish Rabi Kruger, who escaped an occupied Poland. He promised to do all his best for the thousands of Jewish refugees by trying to persuade the government of Lisbon led by dictator Salazar. On that night, Rabi Kruger stayed in the consul’s home.
+On the morning of June 17th Lisbon denied the visas to the Jewish refugees, but unexpectedly Aristides de Sousa Mendes informed the Rabi that he would issue the visas, for he knew that the refugees were doomed to die on the horrific Nazi concentration camps. The house of the family – Casa do Passal located in Cabanas de Viriato, Viseu – was repossessed by the bank and eventually sold to cover debts. The Judaic Association of Lisbon was the only help that the Sousa Mendes family had, providing them with food and medical assistance. Aristides de Sousa Mendes died on April 3rd 1954 in poverty, but he fought for the justice of his deeds until his last breath. As the Nazi threat and the persecution of Jews all over Europe grew stronger and increasingly terrifying, thousands of Jewish refugees in Bordeaux gathered in front of the Portuguese and Spanish consulates, searching for visas in order to escape a certain death. Spain denied the visas to the Jewish refugees and the only hope was the Portuguese consulate.
+</p>
+          <div class="title">The insubordinate consul</div>
+          <p class="paragraph">From June 17th to 19th, the Portuguese consul works relentlessly on visa issuing, along with two of his sons, without even stopping for eating. On those three days 30.000 visas were issued against the orders of dictator António de Oliveira Salazar. The Portuguese consulates in Bayonne and Hendaye have obeyed the orders of Salazar, but Aristides de Sousa Mendes goes to these cities personally and issues more visas. He was aware of the consequences of his deeds, but he followed the demands of his moral conscience. On June 24th 1940 Aristides de Sousa Mendes receives a telegram from Salazar, ordering him to come to Lisbon and to explain his disobedience. Not only was Aristides de Sousa Mendes fired, but he was also denied a retirement, after a 30 years-long diplomatic career. His sons were forbidden to attend university and the Sousa Mendes family would soon lose the house Casa do Passal. The Jewish Community of Lisbon provides shelter and food for the family, helping some of his sons to move to the US and Canada. He dies on April 3rd 1954 in misery.</p>
+          <div class="title">The memory of Aristides de Sousa Mendes</div>
+          <p class="paragraph">The first recognition came in 1966 from Israel, which declared Aristides de Sousa Mendes to be a “Righteous Among the Nations.” In 1986, the United States Congress issued a proclamation honoring his heroic act. Later he was finally recognized by Portugal, when its President Mario Soares apologized to the Sousa Mendes family and the Portuguese Parliament promoted him posthumously to the rank of Ambassador. The face of Aristides de Sousa Mendes has now appeared on postage stamps in several countries.
+“What kind of world is this where you have to be mad to do the right thing?”, Aristides de Sousa Mendes.</p>
+        </div>
+      </body>
+    </div>
+  </content>
+  <footer>
+    <div id="footer">
+      Read more in <a target="_blank" href="https://www.natgeo.pt/historia/aristides-de-sousa-mendes-o-heroi-sem-capa">National Geographic</a> and <a id="tribute-link" target="_blank" href="https://www.centerofportugal.com/pt/tour/aristides-de-sousa-mendes-o-consul-insubordinado">Center of Portugal</a>.
+    </div>
+    <p className='copyright'>All copyright from National Geographic and Center of Portugal.</p>
+  </footer>
+</div>
   );
 }
 
